a9b692b862ab3e800538839d146e11ccc70f9b67
steven.yeo <steven.y@dynrtech.com>
2022-12-08 22:29:15 +0700

    add react build to nodejs

--- app.js
+++ app.js
@@ -12,16 +12,21 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//     next();
+// });
 
 app.use('/api/users', usersRouter);
 app.use('/api/places', placesRouter);
+
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find current endpoint route.', 404);
---
+++ public/precache-manifest.b834badc3e22735057f6ee87b5468a23.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf79cce371988dcdf49abf6fcd7527a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e36103521c6e18f04db3",
+    "url": "/static/css/3.f7faa136.chunk.css"
+  },
+  {
+    "revision": "225a71f59be67baa8b1b",
+    "url": "/static/css/4.565493f7.chunk.css"
+  },
+  {
+    "revision": "7a324ad4e769957b1b76",
+    "url": "/static/css/5.08ca8d85.chunk.css"
+  },
+  {
+    "revision": "5af12e1b31dd1ec61a1d",
+    "url": "/static/css/6.3fd63887.chunk.css"
+  },
+  {
+    "revision": "5ae32de9b9a12528453b",
+    "url": "/static/css/7.a9c5e770.chunk.css"
+  },
+  {
+    "revision": "eb530d8dbb7ee086f56d",
+    "url": "/static/css/main.13f950f0.chunk.css"
+  },
+  {
+    "revision": "7fc2ed2050804cbd4e22",
+    "url": "/static/js/2.e4ca54a0.chunk.js"
+  },
+  {
+    "revision": "e36103521c6e18f04db3",
+    "url": "/static/js/3.d1d23f74.chunk.js"
+  },
+  {
+    "revision": "225a71f59be67baa8b1b",
+    "url": "/static/js/4.6a4e7848.chunk.js"
+  },
+  {
+    "revision": "7a324ad4e769957b1b76",
+    "url": "/static/js/5.042c02bc.chunk.js"
+  },
+  {
+    "revision": "5af12e1b31dd1ec61a1d",
+    "url": "/static/js/6.b98437a2.chunk.js"
+  },
+  {
+    "revision": "5ae32de9b9a12528453b",
+    "url": "/static/js/7.0dbc8718.chunk.js"
+  },
+  {
+    "revision": "eb530d8dbb7ee086f56d",
+    "url": "/static/js/main.9383ce81.chunk.js"
+  },
+  {
+    "revision": "e02760b782a91ee2d625",
+    "url": "/static/js/runtime-main.90b17b52.js"
+  }
+]);
